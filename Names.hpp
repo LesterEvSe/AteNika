5b9c696bf463af4275b9d887a1871bb986041a9d
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2023-07-12 22:24:13 +0300

    Added pieces definition

---
+++ Names.hpp
@@ -0,0 +1,19 @@
+#ifndef CHESSAI_NAMES_HPP
+#define CHESSAI_NAMES_HPP
+
+enum Rank {
+    RANK_1,RANK_2,RANK_3,RANK_4,
+    RANK_5,RANK_6,RANK_7,RANK_8
+};
+
+enum Col {
+    COL_A, COL_B, COL_C, COL_D,
+    COL_E, COL_F, COL_G, COL_H
+};
+
+enum Color {
+    WHITE,
+    BLACK,
+};
+
+#endif //CHESSAI_NAMES_HPP
--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
+#include "Names.hpp"
+
+using U64 = unsigned long long;
+
+class Pawn
+{
+private:
+    U64 pawns;
+
+public:
+
+};
 
 int main() {
 
