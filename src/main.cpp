32738587519cee266ed8320753bad822c4c1d865
LesterEvSe <evgenii.nikolaevichfgh@gmail.com>
2023-10-26 21:03:23 +0300

    Implement piece_square_tables

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include "rays.hpp"
 #include "board.hpp"
 #include "movepicker.hpp"
+#include "piece_square_tables.hpp"
 
 int main() {
     Rays::init();
     Attacks::init(); // Must be init after Rays
     MovePicker::init();
     ZobristHash::init();
+    PieceTables::init();
 
     return 0;
 }
